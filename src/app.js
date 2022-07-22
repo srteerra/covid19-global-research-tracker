346e3b5f34b7fa1a7ec413468d3ac4fcf33fddc2
Angel Lopez <srterradev@gmail.com>
2022-07-22 13:04:45 -0600

    initial structure

---
+++ src/app.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+require("dotenv").config();
+
+// express
+const express = require("express");
+const path = require("path");
+
+// INITIALIZING
+const app = express();
+app.set("port", process.env.PORT || 9000);
+
+// STATIC FILES
+app.use(express.static(path.join(__dirname, "public")));
+
+// RUN SERVER
+app.listen(app.get("port"), () => {
+  console.log("SERVER ON PORT: ", app.get("port"));
+});
---
+++ src/app/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+"use-strict";
+import Vue from "vue";
+import App from "./App.vue";
+
+new Vue({
+  render: (h) => h(App),
+}).$mount("#app");
